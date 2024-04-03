0c119c541078ce3f2622f9a253d069299a9580e8
Bào Ngư <phambao281005@gmail.com>
2024-04-03 21:24:46 +0700

    Update a lot of things

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import Footer from './HOME/Footer/footer';
 import Footer2 from './HOME/Footer2/footer2';
 import Header from './HOME/Header/header';
 import Main from './HOME/MainContent/main';
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 // daay la test
 function App() {
   return (
     <div className="container-fluid">
-      <div className='col-12 position-fixed' style={{marginLeft:'-12px',marginTop:'0px',background:'rgba(255,255,255,0.9)',zIndex:'10'}}>
-        <Header/>
+      <div className='col-12 position-fixed' style={{ marginLeft: '-12px', marginTop: '0px', background: 'rgba(255,255,255,0.9)', zIndex: '10' }}>
+        <Header />
       </div>
-      <div className='col-12' style={{marginTop:'0px'}}>
-        <Main/>
-      </div>
-      <div className='col-12'>
-        <Card/>
+      <div className='col-12' style={{ marginTop: '0px' }}>
+        <Main />
       </div>
       <div className='col-12'>
-        <Footer/>
+        <Card />
       </div>
       <div className='col-12'>
-        <Footer2/>
+        <Footer />
       </div>
+      <div className='col-12'>
+        <Footer2 />
+      </div>
+    </div>
+    <div>
+      <SpeedInsights />
     </div>
   );
 }
